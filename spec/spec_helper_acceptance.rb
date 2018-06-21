cddeba3fb349f594869d0b60eb743225880e27c3
tphoney <tp@puppet.com>
2018-06-21 14:19:48 +0100

    (bugfix) fix req puppet version, tests working

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,10 +2,8 @@ require 'puppet'
 require 'beaker-rspec'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
-require 'beaker-task_helper'
 
 run_puppet_install_helper
-install_ca_certs unless pe_install?
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
 
@@ -19,7 +17,6 @@ RSpec.configure do |c|
 
   # Configure all nodes in nodeset
   c.before :suite do
-    run_puppet_access_login(user: 'admin') if pe_install?
     on(hosts_as('agent'), "ssh-keygen -f .ssh/id_rsa -N ''")
     on(hosts_as('agent'), 'cat .ssh/id_rsa.pub >> .ssh/authorized_keys')
   end
